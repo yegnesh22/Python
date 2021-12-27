91b8f8d710796de62988b40fcb2773a6fa81beb1
yegnesh <yegnesh22@gmail.com>
2021-12-28 04:20:51 +0530

    Cleanup ocr-test
    Signed-off-by: yegnesh <yegnesh22@gmail.com>

--- ocr-test/tess.py
+++ ocr-test/tess.py
@@ -1,8 +1,23 @@
+#!/usr/bin/python
+
 from PIL import Image
 import pytesseract
 
 #image = 'caffeinemark.png'
 image = 'geekbench.png'
 text = pytesseract.image_to_string(Image.open(image), lang="eng")
-print(text)
+lines = text.splitlines()
+
+result = {
+    'Single-Core Score': None,
+    'Multi-Core Score': None,
+}
+
+for index in range(0, len(lines)):
+    line = lines[index]
+    for key in result:
+        if key in line:
+            result[key] = lines[index - 2].strip()
+
+print (result)
 
